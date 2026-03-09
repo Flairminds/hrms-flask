cb52959b6af2f6f6025441624c0cf6a996c62d05
punit-fm <punit.suman@flairminds.com>
2026-03-09 18:13:38 +0530

    leave fix

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -99,11 +99,24 @@ class IgnoreEmployees:
     IGNORE_FOR_ATTENDANCE = [
         'hr@flairminds.com', 
         'hasmukh@flairminds.com', 
+        'barun.sharma@flairminds.com',
+        'yogesh.amritkar@flairminds.com',
+        'shrinivas.sarmane@flairminds.com',
+        'shishir.nigam@flairminds.com'
+    ]
+
+    IGNORE_FOR_REVIEWS = [
+        'hr@flairminds.com', 
+        'hasmukh@flairminds.com', 
         'barun.sharma@flairminds.com', 
-        'yogesh.amritkar@flairminds.com'
+        'yogesh.amritkar@flairminds.com',
+        'shrinivas.sarmane@flairminds.com',
+        'shishir.nigam@flairminds.com'
     ]
 
     IGNORE_FOR_PROJECTS = [
         'hr@flairminds.com',
-        'hasmukh@flairminds.com'
+        'hasmukh@flairminds.com',
+        'priyanka.mandale@flairminds.com',
+        'shishir.nigam@flairminds.com'
     ]
